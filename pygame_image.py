f17f78e3305414e091c5caba4cb2c9e1de99fb42
C0C25024 <c0c250248b@edu.teu.ac.jp>
2026-04-14 15:48:06 +0900

    練習5,6

--- pygame_image.py
+++ pygame_image.py
@@ -16,12 +16,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0]) #練習２
+        x=tmr
+        screen.blit(bg_img, [-x, 0]) #練習２
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
